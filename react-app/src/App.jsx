0fd1b016ea4bde635c8c02faad15d9c483dadb3a
codedbydumi <duminduthushan9@gmail.com>
2025-07-14 13:55:47 +0530

    learn
    refreshing all basics again

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,31 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const name1 = "D"; 
+  const name2 = "T"; 
+  const name3 = "A";
+  const Fname = (a,b,c) => {
+  return  `${a}   ${b}  ${c}`;
 
+
+  } 
+
+  const a =150;
+  const arr = ["apple","bannaa"];
   return (
+
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <h1>Dumindu</h1>
+    <p> {20*10}%  great</p>
+    <p>{a} </p>
+    <p>{a>11 ? "oh yea" : "hell no"}</p>
+
+<p>
+  Full Name  : {Fname ("Dumindu","Thushan","Abhaywickrama")}
+  <br />
+  he likes : {arr[1]}
+</p>
+   </>
   )
 }
 
