c96ec84dcdcccef04a73f604d2dfc4e29304d1ab
codedbydumi <duminduthushan9@gmail.com>
2025-07-14 20:48:16 +0530

    .
    .

--- react-app/src/Componenets/Head.jsx
+++ react-app/src/Componenets/Head.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import './home/Main.css'
+import './Navbar.css'
 
 function Head() {
   return (
     <div>
-  <h1>Header</h1>
+    <nav className="navbar">
+      <div className="navbar-logo">🍎 MyFruitApp</div>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#fruits">Fruits</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+    </nav>
+
     </div>
   )
 }
