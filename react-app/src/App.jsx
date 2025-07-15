5fb5f09af769740dbf6bdebc75fe3bab5d1f28a5
codedbydumi <duminduthushan9@gmail.com>
2025-07-15 14:45:22 +0530

    .
    .

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-// import Body from './Componenets/home/Body'
-// import Head from './Componenets/Head'
-// import Header from './Componenets/home/header'
-// import Footer from './Componenets/home/footer'
+
 
 function App() {
   const products = [
