2f1a66bc9795dbfe89977d96591ce1f3e969beca
codedbydumi <duminduthushan9@gmail.com>
2025-07-15 23:12:43 +0530

    .
    .

--- TasteTales/src/components/common/Footer.jsx
+++ TasteTales/src/components/common/Footer.jsx
@@ -128,7 +128,7 @@ const Footer = () => {
         {/* Bottom Bar */}
         <div className="border-t border-gray-800 mt-8 pt-8 flex flex-col md:flex-row justify-between items-center">
           <div className="text-gray-300 text-sm mb-4 md:mb-0">
-            © 2024 Recipe Finder. All rights reserved.
+            © 2025 Recipe Finder. All rights reserved.
           </div>
           
           {/* Social Links */}
